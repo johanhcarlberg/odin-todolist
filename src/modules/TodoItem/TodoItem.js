128c531fcbcb808dbfdbea8d9d0a8fa0029ce4e3
JSHC <johan.hansson.carlberg@gmail.com>
2022-12-28 11:58:22 +0100

    Add initial project and todoitem classes

---
+++ src/modules/TodoItem/TodoItem.js
@@ -0,0 +1,8 @@
+export class TodoItem {
+    constructor(id, title, description, dueDate) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+    }
+}
