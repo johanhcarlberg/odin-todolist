8a9a92d2ba06d831dfd872429a2c639699c4d944
JSHC <johan.hansson.carlberg@gmail.com>
2022-12-30 09:06:15 +0100

    Add initial page switching logic

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import { addTodoItem, getTodoItemById, getTodoItems } from './modules/TodoItem/TodoItemController';
-import * as TodoItemDetail from './modules/TodoItem/TodoItemDetail';
+import TodoItemDetail from './modules/TodoItem/TodoItemDetail';
+import ProjectList from './modules/Project/ProjectList';
 console.log('TodoList app loaded');
 addTodoItem("Test", "Test Description", "2022-12-29");
 addTodoItem("Test2", "Test Description 2", "2022-12-30");
 console.table(getTodoItems());
 
+const pages = {
+    "TodoItemDetail": TodoItemDetail,
+    "ProjectList": ProjectList,
+}
+
 const mainContent = document.createElement('div');
 mainContent.classList.add('main-content');
 document.body.appendChild(mainContent);
 
-mainContent.appendChild(TodoItemDetail.render(getTodoItemById(1)));
+function changePage(page, data) {
+    console.log({page, data});
+    if (!page) {
+        return;
+    }
+
+    mainContent.innerHTML = "";
+    mainContent.appendChild(page.render(data));
+}
+
+changePage(ProjectList);
+
+
+
+
+
+
+
--- src/modules/TodoItem/TodoItemDetail.js
+++ src/modules/TodoItem/TodoItemDetail.js
@@ -1,17 +1,24 @@
 import { getTodoItemById } from "./TodoItemController";
-
-export function render(todoItem) {
-    const todoItemDiv = document.createElement('div');
-
-    const todoItemHeader = document.createElement('h2');
-    todoItemHeader.textContent = todoItem.title;
-
-    const todoItemContent = document.createElement('div');
+class TodoItemDetail {
+    render(id) {
+        const todoItem = getTodoItemById(id);
+        if (!todoItem) {
+            return;
+        }
+        const todoItemDiv = document.createElement('div');
     
-
-    todoItemDiv.appendChild(todoItemHeader);
-    todoItemDiv.appendChild(todoItemContent);
-    console.log(todoItem);
-
-    return todoItemDiv;
+        const todoItemHeader = document.createElement('h2');
+        todoItemHeader.textContent = todoItem.title;
+    
+        const todoItemContent = document.createElement('div');
+        
+    
+        todoItemDiv.appendChild(todoItemHeader);
+        todoItemDiv.appendChild(todoItemContent);
+        console.log(todoItem);
+    
+        return todoItemDiv;
+    }
 }
+
+export default new TodoItemDetail();
