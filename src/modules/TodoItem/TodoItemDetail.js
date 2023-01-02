c69d00490b8a27d245e40908f14c01b2dfbf38a4
JSHC <johan.hansson.carlberg@gmail.com>
2023-01-02 10:57:18 +0100

    Add link to ProjectDetail from TodoItemDetail

--- src/modules/TodoItem/TodoItemDetail.js
+++ src/modules/TodoItem/TodoItemDetail.js
@@ -1,16 +1,33 @@
 import { getTodoItemById } from "./TodoItemController";
+import ProjectTodoItemMediator from "../ProjectTodoItemMediator";
+import { publishLink } from "../util";
 class TodoItemDetail {
     render(id) {
         const todoItem = getTodoItemById(id);
         if (!todoItem) {
             return;
         }
+        const project = ProjectTodoItemMediator.getProjectById(todoItem.projectId);
         const todoItemDiv = document.createElement('div');
     
         const todoItemHeader = document.createElement('h2');
         todoItemHeader.textContent = todoItem.title;
     
         const todoItemContent = document.createElement('div');
+
+        const projectDiv = document.createElement('div');
+        projectDiv.textContent = 'Project: ';
+        const projectLink = document.createElement('a');
+        projectLink.href = '#';
+        projectLink.textContent = project.name;
+        projectLink.addEventListener('click', (e) => {
+            e.preventDefault();
+            publishLink('ProjectDetail', project.id)
+        });
+        
+        projectDiv.appendChild(projectLink);
+        todoItemContent.appendChild(projectDiv);
+
         const descriptionDiv = document.createElement('div');
         descriptionDiv.textContent = todoItem.description;
         todoItemContent.appendChild(descriptionDiv);
