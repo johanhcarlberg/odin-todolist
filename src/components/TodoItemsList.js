756e72b4cea9b7c4e420ff1c5bf762746071c419
JSHC <johan.hansson.carlberg@gmail.com>
2023-01-13 09:54:54 +0100

    Add TodoItemsList component to ProjectDetail page

--- src/components/TodoItemsList.js
+++ src/components/TodoItemsList.js
@@ -1,5 +1,6 @@
 import { updateTodoItem } from "../modules/TodoItem/TodoItemController";
 import { publishLink } from "../modules/util";
+import './TodoItemsList.css';
 import Checkbox from "./Checkbox";
 
 class TodoItemsList {
@@ -15,18 +16,21 @@ class TodoItemsList {
         const todoItemsList = document.createElement('ul');
         todoItemsList.className = 'todo-items-list';
 
-        for (let todoItem of todoItems) {
+        for (let todoItem of this.todoItems) {
             const todoItemsListItem = document.createElement('li');
             const checkBox = new Checkbox(todoItem.isComplete, () => {
                 todoItem.isComplete = !todoItem.isComplete;
                 updateTodoItem(todoItem);
             });
+            todoItemsListItem.appendChild(checkBox);
 
             const todoItemName = document.createElement('span');
             todoItemName.textContent = todoItem.title;
+            todoItemsListItem.appendChild(todoItemName);
             
             const todoItemDueDate = document.createElement('span');
             todoItemDueDate.textContent = todoItem.dueDate;
+            todoItemsListItem.appendChild(todoItemDueDate);
 
             todoItemsList.appendChild(todoItemsListItem);
         }
--- src/modules/Project/ProjectDetail.js
+++ src/modules/Project/ProjectDetail.js
@@ -1,4 +1,5 @@
 import Checkbox from "../../components/Checkbox";
+import TodoItemsList from "../../components/TodoItemsList";
 import ProjectTodoItemMediator from "../ProjectTodoItemMediator";
 import { TodoItem } from "../TodoItem/TodoItem";
 import { publishLink } from "../util";
@@ -17,49 +18,8 @@ class ProjectDetail {
         projectDetailHeader.textContent = project.name || 'Project Details';
         projectDetailDiv.appendChild(projectDetailHeader);
 
-        const todoItemsContainer = document.createElement('div');
-        todoItemsContainer.classList.add('todo-items-container');
-
-        for (let todoItem of todoItems) {
-
-            const todoItemDiv = document.createElement('div');
-            todoItemDiv.classList.add('todo-item');
-            todoItemDiv.classList.add(`priority-${TodoItem.priorities[todoItem.priority].toLowerCase()}`);
-
-            
-            const todoItemHeader = document.createElement('h3');
-            todoItemHeader.textContent = todoItem.title;
-            todoItemDiv.appendChild(todoItemHeader);
-
-            const todoItemDueDateDiv = document.createElement('div');
-            todoItemDueDateDiv.textContent = `Due date: ${todoItem.dueDate}`;
-            todoItemDiv.appendChild(todoItemDueDateDiv);
-
-            // TODO: Replace with component
-            const checkBoxContainer = document.createElement('div');
-            checkBoxContainer.classList.add('checkBoxContainer');
-            const checkBoxSpan = document.createElement('span');
-            checkBoxSpan.textContent = 'Finished?';
-            const checkbox = new Checkbox(todoItem.isComplete, () => {
-                todoItem.isComplete = !todoItem.isComplete;
-            });
-            checkBoxContainer.appendChild(checkBoxSpan);
-            checkBoxContainer.appendChild(checkbox);
-            todoItemDiv.appendChild(checkBoxContainer);
-
-            todoItemDiv.addEventListener('click', (e) => {
-                e.preventDefault();
-                console.log(e.target);
-                console.log(e.currentTarget);
-                console.log(e.target.tagName);
-                if (e.target === checkbox || e.target.tagName === 'svg') {
-                    return;
-                }
-                publishLink('TodoItemDetail', todoItem.id);
-            })
-
-            todoItemsContainer.appendChild(todoItemDiv);
-        }
+        const todoItemsList = new TodoItemsList(todoItems);
+        projectDetailDiv.appendChild(todoItemsList.render());
         
         const deleteButton = document.createElement('button');
         deleteButton.textContent = 'Delete Project';
@@ -73,8 +33,7 @@ class ProjectDetail {
             deleteProject(project.id);
             publishLink('ProjectList');
         });
-        todoItemsContainer.appendChild(deleteButton);
-        projectDetailDiv.appendChild(todoItemsContainer);
+        projectDetailDiv.appendChild(deleteButton);
         
         return projectDetailDiv;
     }
