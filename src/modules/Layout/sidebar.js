aa96cadba487bd8d0bd8f9043c7779ac2d3635c4
JSHC <johan.hansson.carlberg@gmail.com>
2023-01-13 08:28:17 +0100

    Add main link items to sidebar

--- src/modules/Layout/sidebar.js
+++ src/modules/Layout/sidebar.js
@@ -1,10 +1,29 @@
 import './sidebar.css';
 
 class Sidebar {
+    mainLinks = [
+        'All items',
+        'Today',
+        'Upcoming'
+    ]
     render() {
         const sidebarDiv = document.createElement('div');
         sidebarDiv.className = 'sidebar';
 
+        const mainLinksList = document.createElement('ul');
+        mainLinksList.className = 'sidebar-main-links';
+        for (let link of this.mainLinks) {
+            const mainLinkItem = document.createElement('li');
+            mainLinkItem.className = 'main-link-item';
+            const mainLinkA = document.createElement('a');
+            mainLinkA.href = '#';
+            mainLinkA.text = link;
+            mainLinkItem.appendChild(mainLinkA);
+            
+            mainLinksList.appendChild(mainLinkItem);
+        }
+
+        sidebarDiv.appendChild(mainLinksList);
         return sidebarDiv;
     }
 }
