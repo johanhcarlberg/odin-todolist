303bf474f1946733f825a407f20fe54af5b283c2
JSHC <johan.hansson.carlberg@gmail.com>
2023-01-15 10:10:06 +0100

    Create TodoItemList page with filter for all todo items

---
+++ src/modules/TodoItem/TodoItemList.js
@@ -0,0 +1,30 @@
+import { getTodoItems } from "./TodoItemController"
+
+class TodoItemList {
+    constructor(filter) {
+        this.filter = filters[filter];
+        this.todoItems = this.filter['callback']();
+    }
+
+    static filters = {
+        'All':{
+            'callback':this.allTodoItemsFilter,
+            'title':'All Todo Items'
+        },
+    }
+
+    static allTodoItemsFilter() {
+        return getTodoItems();
+    }
+
+    render() {
+        const todoItemListContainer = document.createElement('div');
+        const todoItemListHeader = document.createElement('h2');
+        todoItemListHeader.textContent = this.filter['title'];
+        todoItemListContainer.appendChild(todoItemListHeader);
+        const todoItemsList = new todoItemsList(this.todoItems);
+        todoItemListContainer.appendChild(todoItemsList);
+
+        return todoItemListContainer;
+    }
+}
