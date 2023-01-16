b568d8289b8d1b2d6e9ad4dec129bd0c28533a44
JSHC <johan.hansson.carlberg@gmail.com>
2023-01-16 09:39:40 +0100

    Remove logging from PubSub

--- src/modules/PubSub.js
+++ src/modules/PubSub.js
@@ -2,25 +2,21 @@ class PubSub {
     events = {};
 
     publish(event, data) {
-        console.log(`publish called for event ${event} with data ${JSON.stringify(data)}`)
         if (!this.events[event]) {
             return;
         }
 
         for (let listener of this.events[event]) {
-            console.log(`calling ${listener} for ${event}`)
             listener(data);
         }
     }
 
     subscribe(event, callback) {
-        console.log(`subscribe: ${event}, ${callback}`)
         if (!this.events[event]) {
             this.events[event] = [];
         }
 
         this.events[event].push(callback);
-        console.log(`${callback} subscribed to ${event}`)
     }
 
     unsubscribe(event, callback) {
