a8b20f22c7fe34b8bb5e9f87ca9fd70666a397ba
JSHC <johan.hansson.carlberg@gmail.com>
2023-01-17 19:28:21 +0100

    Remove old navigation

--- src/modules/Layout/header.js
+++ src/modules/Layout/header.js
@@ -4,9 +4,6 @@ import { publishLink } from "../util";
 import './header.css';
 
 class Header {
-    pageLinks = [
-        {name: 'Projects',page: 'ProjectList'}
-    ]
     render() {
         const header = document.createElement('header');
         const logoContainer = document.createElement('div');
@@ -17,23 +14,6 @@ class Header {
         logoContainer.appendChild(logoText);
 
         header.appendChild(logoContainer);
-        
-        const nav = document.createElement('nav');
-        const links = document.createElement('ul');
-        links.classList.add('header-links');
-        for (let page of this.pageLinks) {
-            const listItem = document.createElement('li');
-            listItem.classList.add('header-link-item');
-
-            const listItemLink = document.createElement('a');
-            listItemLink.href = '#';
-            listItemLink.textContent = page.name;
-            listItemLink.addEventListener('click', (e) => this.onHeaderLinkClick(e, page));
-            listItem.appendChild(listItemLink);
-
-            links.appendChild(listItem);
-        }
-        nav.appendChild(links);
 
         const buttonsContainer = document.createElement('div');
         buttonsContainer.classList.add('buttons-container');
@@ -41,8 +21,6 @@ class Header {
         const addComponent = new AddComponent();
         
         buttonsContainer.appendChild(addComponent.render());
-        
-        header.appendChild(nav);
         header.appendChild(buttonsContainer);
         return header;
     }
