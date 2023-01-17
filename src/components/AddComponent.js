94b45d32ec64a8f9e5520ad26e851a192e71ac44
JSHC <johan.hansson.carlberg@gmail.com>
2023-01-17 09:03:20 +0100

    Create Add component

---
+++ src/components/AddComponent.js
@@ -0,0 +1,9 @@
+export default class AddComponent {
+    constructor() {}
+    render() {
+        const addContainer = document.createElement('div');
+        addContainer.className = 'add-container';
+
+        return addContainer;
+    }
+}
