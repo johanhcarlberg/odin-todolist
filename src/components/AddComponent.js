838d6ce000ff5103487e6f7c6e2dca34c67d79b1
JSHC <johan.hansson.carlberg@gmail.com>
2023-01-17 09:05:53 +0100

    Add plus icon to AddComponent

--- src/components/AddComponent.js
+++ src/components/AddComponent.js
@@ -1,8 +1,16 @@
+import './AddComponent.css';
+import addIconSvg from '../../assets/icons/plus.svg';
 export default class AddComponent {
     constructor() {}
     render() {
         const addContainer = document.createElement('div');
         addContainer.className = 'add-container';
+
+        const addIcon = document.createElement('i');
+        addIcon.innerHTML = addIconSvg;
+        addIcon.className = 'add-icon';
+
+        
 
         return addContainer;
     }
