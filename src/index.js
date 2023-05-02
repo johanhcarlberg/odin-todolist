25a7733809881cc5de20828b980aef8ba9c6dd03
JSHC <johan.hansson.carlberg@gmail.com>
2023-05-02 10:37:38 +0200

    Wrap index in async function, update changePage to async

--- src/index.js
+++ src/index.js
@@ -7,41 +7,50 @@ import ProjectDetail from './modules/Project/ProjectDetail';
 import AddTodoItem from './modules/TodoItem/AddTodoItem';
 import AddProject from './modules/Project/AddProject';
 import TodoItemList from './modules/TodoItem/TodoItemList';
+import { initializeApp } from 'firebase/app';
+import firebaseConfig from '../firebase_config';
 import './style.css';
-if (getTodoItems().length === 0) {
-    addTodoItem("Test", 3, "Test Description", "2022-12-29");
-    addTodoItem("Test2", 3, "Test Description 2", "2022-12-30");
-}
 
-const pages = {
-    "TodoItemDetail": TodoItemDetail,
-    "ProjectDetail":ProjectDetail,
-    "AddTodoItem":AddTodoItem,
-    "AddProject":AddProject,
-    'TodoItemList':TodoItemList,
-}
-
-PubSub.subscribe('changePage', changePage);
-
-document.body.appendChild(Header.render());
-document.body.appendChild(Sidebar.render());
-const mainContent = document.createElement('div');
-mainContent.classList.add('main-content');
-document.body.appendChild(mainContent);
-
-function changePage(data) {
-    if (!data.page || !pages[data.page]) {
-        return;
+(async () => {
+    if (getTodoItems().length === 0) {
+        addTodoItem("Test", 3, "Test Description", "2022-12-29");
+        addTodoItem("Test2", 3, "Test Description 2", "2022-12-30");
     }
+    
+    const pages = {
+        "TodoItemDetail": TodoItemDetail,
+        "ProjectDetail":ProjectDetail,
+        "AddTodoItem":AddTodoItem,
+        "AddProject":AddProject,
+        'TodoItemList':TodoItemList,
+    }
+    
+    PubSub.subscribe('changePage', changePage);
+    
+    document.body.appendChild(Header.render());
+    document.body.appendChild(Sidebar.render());
+    const mainContent = document.createElement('div');
+    mainContent.classList.add('main-content');
+    document.body.appendChild(mainContent);
+    
+    async function changePage(data) {
+        if (!data.page || !pages[data.page]) {
+            return;
+        }
+    
+        const element = await pages[data.page].render(data.data);
+        mainContent.innerHTML = "";
+        mainContent.appendChild(element);
+    }
+    
+    await changePage({
+        page: "TodoItemList",
+        data: {title:'All items', callback:getTodoItems}
+    });
+    
+    const app = initializeApp(firebaseConfig);
+})()
 
-    mainContent.innerHTML = "";
-    mainContent.appendChild(pages[data.page].render(data.data));
-}
-
-changePage({
-    page: "TodoItemList",
-    data: {title:'All items', callback:getTodoItems}
-});
 
 
 
