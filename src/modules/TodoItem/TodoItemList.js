ffa780367544ea89bfbd39321909d8192e511268
JSHC <johan.hansson.carlberg@gmail.com>
2023-05-02 19:19:04 +0200

    Add functionality for re-rendering todo item list when todo items change

--- src/modules/TodoItem/TodoItemList.js
+++ src/modules/TodoItem/TodoItemList.js
@@ -1,17 +1,30 @@
 import TodoItemsList from "../../components/TodoItemsList";
+import PubSub from "../PubSub";
 
 class TodoItemList {
     async render({title, callback}) {
         this.todoItems = await callback();
+        this.todoItemListContainer = document.createElement('div');
+        this.renderItemsList(title);
 
-        const todoItemListContainer = document.createElement('div');
+        PubSub.subscribe('TodoItemsChanged', async () => {
+            this.todoItems = await callback();
+            this.todoItemListContainer.innerHTML = '';
+            this.renderItemsList(title);
+        })
+
+        
+        
+
+        return this.todoItemListContainer;
+    }
+
+    renderItemsList(title) {
         const todoItemListHeader = document.createElement('h2');
         todoItemListHeader.textContent = title;
-        todoItemListContainer.appendChild(todoItemListHeader);
-        const todoItemsList = new TodoItemsList(this.todoItems).render();
-        todoItemListContainer.appendChild(todoItemsList);
-
-        return todoItemListContainer;
+        this.todoItemListContainer.appendChild(todoItemListHeader);
+        const todoItemsList = new TodoItemsList(this.todoItems).render();   
+        this.todoItemListContainer.appendChild(todoItemsList);
     }
 }
 
