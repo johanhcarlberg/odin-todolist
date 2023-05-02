6d78651868b4979f180ca683aa4defa610913332
JSHC <johan.hansson.carlberg@gmail.com>
2023-05-02 15:05:44 +0200

    Add function call to add default project when auth state changes

--- src/modules/Project/ProjectController.js
+++ src/modules/Project/ProjectController.js
@@ -1,42 +1,48 @@
 import { Project } from "./Project";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { getNextId, getIndexFromId } from "../util";
 import PubSub from "../PubSub";
 import ProjectRepository from "./ProjectRepository";
 import ProjectRepositoryLocal from "./ProjectRepositoryLocal";
 
-const repository = ProjectRepositoryLocal;
+    let repository = getAuth().currentUser ? ProjectRepository : ProjectRepositoryLocal;
 
-const addProject = async (name) => {
-    const newProject = new Project('', name);
-    const addedProject = await repository.addProject(newProject);
-    PubSub.publish('ProjectChanged');
-    return addedProject;
-}
-
-const getProjects = async () => {
-    const projects = await repository.getProjects();
-    return projects;
-}
-
-const getProjectById = async (id) => {
-    const project = await repository.getProjectById(id);
-    return project;
-}
-
-const deleteProject = async (id) => {
-    await repository.deleteProject(id);
-    PubSub.publish('ProjectChanged');
-}
-
-const addDefaultProject = async () => {
-    const projects = await getProjects();
-    console.log(projects);
-    if (projects.length === 0) {
-        addProject('Default');
+    export const addProject = async (name) => {
+        const newProject = new Project('', name);
+        const addedProject = await repository.addProject(newProject);
+        PubSub.publish('ProjectChanged');
+        return addedProject;
     }
-};
+    
+    export const getProjects = async () => {
+        const projects = await repository.getProjects();
+        return projects;
+    }
+    
+    export const getProjectById = async (id) => {
+        const project = await repository.getProjectById(id);
+        return project;
+    }
+    
+    export const deleteProject = async (id) => {
+        await repository.deleteProject(id);
+        PubSub.publish('ProjectChanged');
+    }
+    
+    export const addDefaultProject = async () => {
+        const projects = await getProjects();
+        if (projects.length === 0) {
+            await addProject('Default');
+        }
+    };
+    
+    addDefaultProject();
 
-addDefaultProject();
+    onAuthStateChanged(getAuth(), (user) => {
+        repository = user ? ProjectRepository : ProjectRepositoryLocal;
+        addDefaultProject();
+        PubSub.publish('ProjectChanged');
+    })
 
 
-export { addProject, getProjects, getProjectById, deleteProject }
+
