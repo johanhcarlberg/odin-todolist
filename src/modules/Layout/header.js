9a2c3e6eb2d39400259d593da4c34070f26a76b7
JSHC <johan.hansson.carlberg@gmail.com>
2023-05-02 13:28:32 +0200

    Add user div and logic for authenticating users

--- src/modules/Layout/header.js
+++ src/modules/Layout/header.js
@@ -4,6 +4,9 @@ import { publishLink } from "../util";
 import logoIconSvg from '../../../assets/icons/clipboard-list-outline.svg';
 import './header.css';
 import { getTodoItems } from "../TodoItem/TodoItemController";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import SignInButton from "../../components/SignInButton";
+import SignOutButton from "../../components/SignOutButton";
 
 class Header {
     render() {
@@ -33,6 +36,34 @@ class Header {
         
         buttonsContainer.appendChild(addComponent.render());
         header.appendChild(buttonsContainer);
+
+        const userContainer = document.createElement('div');
+        userContainer.classList.add('user-container');
+
+        const auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+            console.log('onAuthStateChanged');
+            userContainer.innerHTML = '';
+            if (user) {
+                userContainer.textContent = user.displayName;
+                const signOutButton = SignOutButton().render();
+                userContainer.appendChild(signOutButton);
+            } else {
+                const signInButton = SignInButton().render();
+                userContainer.appendChild(signInButton);
+            }
+        })
+
+        if (auth.currentUser) {
+            userContainer.textContent = auth.currentUser.displayName;
+            userContainer.appendChild(signOutButton);
+        } else {
+            const signInButton = SignInButton().render();
+            userContainer.appendChild(signInButton);
+        }
+
+        header.appendChild(userContainer);
+
         return header;
     }
 
